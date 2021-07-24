274fa8a67fa5d3cbb535b9a7420b03292a4a51af
hamada456 <n87.takaya.hamada@gmail.com>
2021-07-24 11:12:46 +0900

    リファクタリング

--- ui.js
+++ ui.js
@@ -5,6 +5,8 @@
     const $tab = $doc.getElementById("js-tab");
     const $nav = $tab.querySelectorAll("[data-nav]");
     const $content = $tab.querySelectorAll("[data-content]");
+    const ACTIVE_CLASS = "is-active";//書き換える時にここ一箇所で済む
+    const navLen = $nav.length;
 
     //初期化(initialize)最初に実行したい
     const init = () => {
@@ -22,24 +24,23 @@
 
         //対象外のnav,content全て一旦リセットする
         let index = 0;
-        while(index < $nav.length){
+        while(index < navLen){
             $content[index].style.display = "none";
-            $nav[index].classList.remove("is-active");
+            $nav[index].classList.remove(ACTIVE_CLASS);
             index++;
         }
 
         //対象のコンテンツをアクティブ化する
         $tab.querySelectorAll("[data-content='" + targetVal + "']")[0].style.display = "block";
-        $nav[targetVal].classList.add("is-active");
+        $nav[targetVal].classList.add(ACTIVE_CLASS);
 
     };
 
     //全nav要素に対して関数を適応・発火
     let index = 0;
-    while(index < $nav.length){
+    while(index < navLen){
         $nav[index].addEventListener("click", (e) => handlerClick(e));
         index++;
     }
     
-
 })();
